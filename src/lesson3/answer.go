ed12f4d02204d918a38d2bf474f65d6b0636beba
zakimiiii <mk2.tg000@gmail.com>
2021-12-27 12:05:16 +0900

    add 3.2

--- src/lesson3/answer.go
+++ src/lesson3/answer.go
@@ -1,6 +1,9 @@
 package lesson3
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 // 3.1
 type FixedMultiStack struct {
@@ -59,4 +62,42 @@ func (s *FixedMultiStack) isFull(num int) bool {
 
 func (s *FixedMultiStack) idxOfTop(num int) int {
 	return s.sizes[num] + s.stackCap*num - 1
+}
+
+// 3.2
+type StackWithMin struct {
+	stack    MyStack
+	minStack MyStack
+}
+
+func NewStackWithMin() StackWithMin {
+	return StackWithMin{
+		stack:    NewMyStack(nil),
+		minStack: NewMyStack(nil),
+	}
+}
+
+func (s *StackWithMin) push(data int) {
+	s.stack.push(data)
+	if data <= s.min() {
+		s.minStack.push(data)
+	}
+}
+
+func (s *StackWithMin) pop() int {
+	n, _ := s.stack.pop()
+	if n.data == s.min() {
+		s.minStack.pop()
+	}
+
+	return n.data.(int)
+}
+
+func (s *StackWithMin) min() int {
+	if s.minStack.isEmpty() {
+		return math.MaxInt
+	}
+
+	val, _ := s.minStack.peek()
+	return val.(int)
 }
