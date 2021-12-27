8bbda78b499ec4654255ea1ef5029da05cc2016f
zakimiiii <mk2.tg000@gmail.com>
2021-12-27 10:27:52 +0900

    add a queue using list

--- src/lesson3/queue.go
+++ src/lesson3/queue.go
@@ -47,3 +47,58 @@ func (q *MyQueue) peek() interface{} {
 func (q *MyQueue) isEmpty() bool {
 	return q.first == nil
 }
+
+type MyQueueUseList struct {
+	head  int
+	tail  int
+	max   int
+	queue []interface{}
+}
+
+func NewMyQueueUseList(max int) MyQueueUseList {
+	return MyQueueUseList{
+		max:   max,
+		queue: make([]interface{}, max),
+	}
+}
+
+func (q *MyQueueUseList) isFull() bool {
+	return q.head == (q.tail+1)%q.max
+}
+
+func (q *MyQueueUseList) add(data interface{}) {
+	if q.isFull() {
+		return
+	}
+
+	q.queue[q.tail] = data
+	q.tail++
+	if q.tail == q.max {
+		q.tail = 0
+	}
+}
+
+func (q *MyQueueUseList) remove() interface{} {
+	if q.isEmpty() {
+		return nil
+	}
+
+	data := q.queue[q.head]
+	q.head++
+	if q.head == q.max {
+		q.head = 0
+	}
+	return data
+}
+
+func (q *MyQueueUseList) peek() interface{} {
+	if q.isEmpty() {
+		return nil
+	}
+
+	return q.queue[q.head]
+}
+
+func (q *MyQueueUseList) isEmpty() bool {
+	return q.head == q.tail
+}
