c31dad927b20942214bd7c39f7bcba9afc41c092
zakimiiii <mk2.tg000@gmail.com>
2021-12-27 10:06:33 +0900

    add queue

--- src/lesson3/queue.go
+++ src/lesson3/queue.go
@@ -1,1 +1,49 @@
 package lesson3
+
+type MyQueue struct {
+	top   *Node
+	first *Node
+	last  *Node
+	data  interface{}
+}
+
+func NewMyQueue(data interface{}) MyQueue {
+	return MyQueue{data: data}
+}
+
+func (q *MyQueue) add(data interface{}) {
+	node := &Node{data: data}
+	if q.last != nil {
+		q.last.next = node
+	}
+	q.last = node
+
+	if q.first == nil {
+		q.first = node
+	}
+}
+
+func (q *MyQueue) remove() interface{} {
+	if q.first == nil {
+		return nil
+	}
+
+	data := q.first.data
+	q.first = q.first.next
+	if q.first == nil {
+		q.last = nil
+	}
+	return data
+}
+
+func (q *MyQueue) peek() interface{} {
+	if q.first == nil {
+		return nil
+	}
+
+	return q.first.data
+}
+
+func (q *MyQueue) isEmpty() bool {
+	return q.first == nil
+}
