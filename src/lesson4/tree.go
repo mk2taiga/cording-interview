3b64383473ff15eb9d3d9e5de762ce191fa46cc8
zakimiiii <mk2.tg000@gmail.com>
2021-12-28 14:50:50 +0900

    bin tree

--- src/lesson4/tree.go
+++ src/lesson4/tree.go
@@ -81,3 +81,102 @@ func (t Tree) postParse(u *Node) {
 	t.preParse(u.right)
 	println(u.val)
 }
+
+type BinTree struct {
+	root *Node
+}
+
+func (t BinTree) insert(val int) {
+	newNode := &Node{val: val}
+
+	var leaf *Node
+	child := t.root
+	for child != nil {
+		leaf = child
+		if newNode.val < child.val {
+			child = child.left
+		} else {
+			child = child.right
+		}
+	}
+
+	newNode.parent = leaf
+	if leaf == nil {
+		t.root = newNode
+	} else {
+		if newNode.val < leaf.val {
+			leaf.right = newNode
+		} else {
+			leaf.left = newNode
+		}
+	}
+}
+
+func (t BinTree) find(val int) *Node {
+	u := t.root
+	for u != nil && u.val != val {
+		if val < u.val {
+			u = u.left
+		} else {
+			u = u.right
+		}
+	}
+
+	return u
+}
+
+func (t BinTree) treeMin(u *Node) *Node {
+	for u.left != nil {
+		u = u.left
+	}
+	return u
+}
+
+func (t BinTree) treeSuccessor(u *Node) *Node {
+	if u.right != nil {
+		return t.treeMin(u)
+	}
+
+	p := u.parent
+	for p != nil && p.right == u {
+		u = p
+		p = p.parent
+	}
+
+	return p
+}
+
+func (t BinTree) delete(u *Node) {
+	var target *Node
+	var targetChild *Node
+
+	if u.left == nil || u.right == nil {
+		target = u
+	} else {
+		target = t.treeSuccessor(u)
+	}
+
+	if target.left != nil {
+		targetChild = target.left
+	} else {
+		targetChild = target.right
+	}
+
+	if targetChild != nil {
+		targetChild.parent = target.parent
+	}
+
+	if target.parent == nil {
+		t.root = targetChild
+	} else {
+		if target == target.parent.left {
+			target.parent.left = targetChild
+		} else {
+			target.parent.right = targetChild
+		}
+	}
+
+	if target != u {
+		u.val = target.val
+	}
+}
